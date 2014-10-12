9588b326cc5cc2407f6b2bf206a5c4e112c13c03
Vijay Chandar <reachvijaychandar@gmail.com>
2014-10-12 21:26:30 +0530

    Update fact.cpp

--- fact.cpp
+++ fact.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-int n;
-cin>>n;
-int f[200];
-f[1]=1;
-m=1;
-int temp=0;
-for(int i=1;i<=n;i++)
-{
-f[i]=f[i]*i;
-}
-cout<<"factorial: "<<f[]<<endl;
-return 0;
+  int n;
+  cin>>n;
+  int f[200];
+  f[1]=1;
+  m=1;
+  int temp=0;
+  for(int i=1;i<=n;i++)
+  {
+    f[i]=f[i]*i;
+  }
+  cout<<"factorial: "<<f[]<<endl;
+  return 0;
 }
